610be065cdb7e59086451f767564602525d6c459
ukemeSkywalker <skywalkershadow9@gmail.com>
2022-08-19 21:10:10 +0100

    Readme update

--- scripts/deployNFT.ts
+++ scripts/deployNFT.ts
@@ -11,8 +11,15 @@ async function main() {
 
 }
 
-// NFT deployed at: 0x7BEbC67d54D5Be6949dBA7A83cA33B251F201608
+
 // NFT deployed at: 0x0B5f0Cdb79eDAF192B3c19cd9Ddc5A3f1772017E
+
+
+// Rinkeby EOA Address: 0xEd521548b91C5e4f58bCc1f30dE9691eff517048
+ // Tnx Hash: "0x2cde98bd0e785b4088b3e0ef3e041db6a6b0ff9a23c8de4056ad5f602967a6d2"
+// OpenSea: https://testnets.opensea.io/assets/rinkeby/0x0b5f0cdb79edaf192b3c19cd9ddc5a3f1772017e/1
+
+
 
 main().catch((error) => {
     console.error(error);
--- scripts/interactNFT.ts
+++ scripts/interactNFT.ts
@@ -3,23 +3,23 @@ import { ethers } from "hardhat";
 async function main() {
 
     const NFTdeploy = ethers.getContractFactory("WakandaForever");
-    const CONTRACTADDRESS = "0x7BEbC67d54D5Be6949dBA7A83cA33B251F201608";
+
     const CONTRACTADDRESS2 = "0x0B5f0Cdb79eDAF192B3c19cd9Ddc5A3f1772017E";
 
-    const URI = "ipfs://QmeirtN78yF3fL2tDDJPvVfJBAkMP7NVyhw26RCxvpaGjE";
-    const URI2 ="https://ipfs.filebase.io/ipfs/QmeirtN78yF3fL2tDDJPvVfJBAkMP7NVyhw26RCxvpaGjE";
+    const URL = "https://gateway.pinata.cloud/ipfs/QmRjYbiN87LU1zaRZ3a1Znk77EgRW8pbVzvcVn1jh3jwRj";
 
     const userAddress = "0xEd521548b91C5e4f58bCc1f30dE9691eff517048"
 
     const interact = await (await NFTdeploy).attach(CONTRACTADDRESS2);
 
-    const mint = interact.safeMint(userAddress, URI2);
+    const mint = interact.safeMint(userAddress, URL);
     const mintResult = await (await mint);
 
     console.log("mint successfull:", mintResult);
 
-    // hash: '0xaa0ef394db2961e663fb6116d49a0d6f971c8bb0fcb3bd933503ade9ffff643
-    //  hash: '0xa09c397a74b452ee435f415fd596f432261689cd4b26bf72433aebbff47c1ba0'
+    // Rinkeby EOA Address: 0xEd521548b91C5e4f58bCc1f30dE9691eff517048
+    // Tnx Hash: "0x2cde98bd0e785b4088b3e0ef3e041db6a6b0ff9a23c8de4056ad5f602967a6d2"
+    // OpenSea: https://testnets.opensea.io/assets/rinkeby/0x0b5f0cdb79edaf192b3c19cd9ddc5a3f1772017e/1
 
 
 }
